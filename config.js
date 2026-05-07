58d064f0d86185e922d6e259177c9f3c8b9c4e61
dulcebebauy <dulcebeba.uy@gmail.com>
2026-05-07 17:26:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
 const API_DES = "https://script.google.com/macros/s/AKfycbyLNPKzt0ROtLnBE5ICTZS9hGfFmpc5GEEFmw3W5qhNMMt_gK5GoqdwAYL23MmzicZxdw/exec";
+
+function getAPI( ambiente = "desa"){
+  if(ambiente.lower() == "desa"){
+    return API_DESA;
+  }
+  if((ambiente.lower() == "prod"){
+    return API_PROD;
+  }
+  return false;
+}
