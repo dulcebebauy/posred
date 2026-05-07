57ba2f77fce2f74246d8ad35982a54d7ba211130
dulcebebauy <dulcebeba.uy@gmail.com>
2026-05-07 17:42:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-const API_DESA = "https://script.google.com/macros/s/AKfycbyLNPKzt0ROtLnBE5ICTZS9hGfFmpc5GEEFmw3W5qhNMMt_gK5GoqdwAYL23MmzicZxdw/exec";
+const API_DESA = "https://script.google.com/macros/s/AKfycbw5B721kgIQ32F-NIMlrU351lg-gSTsg0fqJVsXk8xesHYh6pn7MtQbjiTi47QquuTy/exec"; 
+                //"https://script.google.com/macros/s/AKfycbyLNPKzt0ROtLnBE5ICTZS9hGfFmpc5GEEFmw3W5qhNMMt_gK5GoqdwAYL23MmzicZxdw/exec";
 
 function getAPI( ambiente = "desa"){
   if(ambiente.toLowerCase() == "desa"){
