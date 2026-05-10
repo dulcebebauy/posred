d5da3274c353d30f030d0386d31e78732c2549bc
dulcebebauy <dulcebeba.uy@gmail.com>
2026-05-10 19:39:59 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-const API_DESA = "https://script.google.com/macros/s/AKfycbyj8MdsIRsp4ng3kqutTipyO7qU3clznj4xNTapF-RKifumENBbhZWp96-LqVxNtt-MMw/exec"; 
+const API_DESA = "https://script.google.com/macros/s/AKfycbz8UOeF91ZZHePeVjAzVDN9IZL-yBLjgJ9xcZ1p5OvLSckPHrnS2_CzRbcmRXR8IZVLGw/exec";
+                //"https://script.google.com/macros/s/AKfycbyj8MdsIRsp4ng3kqutTipyO7qU3clznj4xNTapF-RKifumENBbhZWp96-LqVxNtt-MMw/exec"; 
                 //"https://script.google.com/macros/s/AKfycbzrWddU4SlnuaBfba45-rtqH5FKm5Nv3-JvhupAcZ-v_kS6HQGWwT4LrZ4ul8VB8VbJHg/exec";
                 //"https://script.google.com/macros/s/AKfycbw5B721kgIQ32F-NIMlrU351lg-gSTsg0fqJVsXk8xesHYh6pn7MtQbjiTi47QquuTy/exec"; 
                 //"https://script.google.com/macros/s/AKfycbyLNPKzt0ROtLnBE5ICTZS9hGfFmpc5GEEFmw3W5qhNMMt_gK5GoqdwAYL23MmzicZxdw/exec";
